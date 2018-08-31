749843a212bcd19589a93e0c68121e24666102a1
Apoorv Patne <31184004+apoorvpatne10@users.noreply.github.com>
2018-08-31 19:37:44 +0530

    Update mnist_loader.py

--- handwritten-digits-recognition/mnist_loader.py
+++ handwritten-digits-recognition/mnist_loader.py
@@ -5,7 +5,7 @@ import gzip
 import numpy as np
 
 def load_data():
-    f = gzip.open('data2/mnist.pkl.gz', 'rb')
+    f = gzip.open('Data/mnist.pkl.gz', 'rb')
     training_data, validation_data, test_data = pickle.load(f, encoding='latin1')
     f.close()
     return (training_data, validation_data, test_data)
